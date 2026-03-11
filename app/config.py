b75358ccac04fee9a9b779162833ec78fa9ce033
Amelie Artmann <amelie.artmann@l3montree.com>
2026-03-11 09:32:29 +0100

    fix db authentication error

--- app/config.py
+++ app/config.py
@@ -10,12 +10,12 @@ OVERLAP_SIZE = 120 # overlap rule of thumb: around 10-20% of chunk size
 BATCH_SIZE = 50
 
 MODEL_EMBEDDING = "mistral-embed"
-MODEL_GENERATION = "mistral-small-2506"
+MODEL_GENERATION = "mistral-medium-2508" # alternatively use: "mistral-small-2506"
 
-DB_NAME = "embedding_db"
-DB_HOST = "localhost"
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = "db"
 DB_PORT = 5432
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 API_KEY = os.getenv("API_KEY")
