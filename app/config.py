1fdba2ae70fe1e63f82806ebd09a1d51ec01c576
Sebastian Kawelke <sebastian.kawelke@l3montree.com>
2026-03-11 17:34:52 +0100

    Changes to read db port and host from env
    Signed-off-by: Sebastian Kawelke <sebastian.kawelke@l3montree.com>

--- app/config.py
+++ app/config.py
@@ -13,8 +13,8 @@ MODEL_EMBEDDING = "mistral-embed"
 MODEL_GENERATION = "mistral-medium-2508" # alternatively use: "mistral-small-2506"
 
 DB_NAME = os.getenv("POSTGRES_DB")
-DB_HOST = "db"
-DB_PORT = 5432
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
